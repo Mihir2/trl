209dc4cddaf2006fa0af1064aabe4ff1bc50d100
Mihir Chauhan <mihirchauhan9@gmail.com>
2023-11-01 16:46:47 -0700

    test

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -431,9 +431,12 @@ class DPOTrainer(Trainer):
         print('-----')
 
         per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
-        print('---Per token logps ----')
+        print('---Logits softmax ----')
         print(logits.log_softmax(-1).shape)
         print(logits.log_softmax(-1))
+        print('-----')
+        print('--- per_token_logps ----')
+        print(per_token_logps.shape)
         print('-----')
         if average_log_prob:
             return (per_token_logps * loss_mask).sum(-1) / loss_mask.sum(-1)
